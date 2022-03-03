f7a6d1667a3887612211542bde61fd41e70b32cc
yossi1118n2 <yossimaster1401@gmail.com>
2022-03-03 20:42:40 +0900

    providerを使った書き換えを検討

--- lib/GamePage.dart
+++ lib/GamePage.dart
@@ -4,7 +4,13 @@ import 'package:provider/provider.dart';
 import 'Game/NoGame.dart';
 import 'Game/ProgressGame.dart';
 
-
+//TODO: 画面遷移の時にデータをどうやってやり取りするかを考え中
+//今のところProviderをうまく使えないかを考えている
+//そのための参考記事としてこちらをみている途中
+//https://rinoguchi.net/2021/05/share-state-between-other-routes.html
+//githubのコードを読んでいる(以下の2つ)
+//https://github.com/rinoguchi/study_timer/blob/0238aa987e11e17e48ee7673a529ffe6d9d796cd/lib/main.dart
+//https://github.com/rinoguchi/study_timer/blob/0238aa987e11e17e48ee7673a529ffe6d9d796cd/lib/timeKeeper.dart
 class GameData extends ChangeNotifier{
   int _gameStatus = 0;
 
